138d9de710629f09d53b21938a0e2acb6ef97a52
Diego Alonso Archila Torres <daat3523@gmail.com>
2022-05-13 08:28:16 -0500

    [MERGE] con cambios entrantes

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,48 +1,78 @@
- import { Link } from "react-router-dom";
- 
- function Login() {
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
 
   const data = {
-    email: document.getElementById("#login"),
-    pwd: document.getElementById("#pwd")
-  }
+    emaild: email,
+    pwd: pwd,
+  };
 
   return (
-    <form method="POST" action="/api/login" id="Login"  className="mt-5 col-sm-12 col-md-8 d-flex flex-column" onSubmit={e => {
+    <form
+      method="POST"
+      action="/api/login"
+      id="Login"
+      className="mt-5 col-sm-12 col-md-8 d-flex flex-column"
+      onSubmit={(e) => {
         e.preventDefault();
-        console.log(data);
-    }}>
-
-    <div className="row mb-4 justify-content-center">
+        console.log(e);
+      }}
+    >
+      <div className="row mb-4 justify-content-center">
         <div className="col text-center">
           <span className="h4 text-black-50">Iniciar sesión</span>
         </div>
-    </div>
+      </div>
 
-    <div className="form-outline mb-2">
-      <input type="email" id="email" name="email" className="form-control  text-center rounded-lg" style={{fontSize:"16px"}} required/>
-      <label className="form-label h6 mt-3 text-black-50" for="form2Example1">Email</label>
-    </div>
+      <div className="form-outline mb-2">
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="form-control  text-center rounded-lg"
+          style={{ fontSize: "16px" }}
+          required
+          onChange={e=> setEmail(e.target.value) }
+        />
+        <label className="form-label h6 mt-3 text-black-50" for="email">
+          Email
+        </label>
+      </div>
 
-    <div className="form-outline mb-2">
-      <input type="password" id="pwd" name="pwd" className="form-control  text-center rounded-lg" style={{fontSize:"16px"}} required/>
-      <label className="form-label h6 mt-3 text-black-50" for="form2Example2">Password</label>
-    </div>
-        
-    <div className="row justify-content-center">
+      <div className="form-outline mb-2">
+        <input
+          type="password"
+          id="pwd"
+          name="pwd"
+          className="form-control  text-center rounded-lg"
+          style={{ fontSize: "16px" }}
+          required
+          onChange={e=> setPwd(e.target.value) }
+        />
+        <label className="form-label h6 mt-3 text-black-50" for="pwd">
+          Password
+        </label>
+      </div>
+
+      <div className="row justify-content-center">
         <button type="submit" className="btn bg-dark mb-4 rounded text-white">
-            Logueame !
+          Logueame !
         </button>
-    </div>
+      </div>
 
-    <div className="row mb-4 justify-content-center">
+      <div className="row mb-4 justify-content-center">
         <div className="col text-center">
-            <Link to="/user/create" className="h6">¿No tienes una cuenta? <span className="text-danger h5"> crea una!</span></Link>
+          <Link to="/user/create" className="h6">
+            ¿No tienes una cuenta?{" "}
+            <span className="text-danger h5"> crea una!</span>
+          </Link>
         </div>
-    </div>
-
-  </form>
+      </div>
+    </form>
   );
- }
+}
 
- export default Login;
+export default Login;
